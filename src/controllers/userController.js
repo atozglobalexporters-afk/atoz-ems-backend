9373cfd8ad54d684adef5021e91fd914b5097809
atozglobalexporters-afk <atozglobalexporters@gmail.com>
2026-04-21 00:52:05 +0530

    fix bcrypt in userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 // src/controllers/userController.js
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { PrismaClient } = require('@prisma/client');
 const { createAuditLog } = require('../services/auditService');
 
